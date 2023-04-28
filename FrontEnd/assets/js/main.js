527519143b2a44b3ef4007f6be64f7e38a4965d8
Siyanne <sirayanne.ket@gmail.com>
2023-04-28 17:35:39 +0200

    categorie-objets

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -4,7 +4,8 @@ const works = await fetchWorks();
 const categories = await fetchCategories();
 
 const galleryElm = document.querySelector(".gallery");
-const boutonFiltrees = document.querySelector(".btn-trier-categories");
+const btnFiltreesEverything = document.querySelector(".btn-trier-tous");
+const btnFiltres = document.querySelector(".filtres");
 
 function createAppend(elm, tag) {
   return elm.appendChild(document.createElement(tag));
@@ -22,14 +23,41 @@ function generateWork(work) {
 
 function generateWorks(works) {
   for (let work of works) generateWork(work);
+  document.querySelector(".gallery").innerHTML = " ";
 }
 
-boutonFiltrees.addEventListener("click", function () {
-  document.querySelector(".filtres");
-  generateWorks(
-    fetchCategories.filter(function (categories) {
-      return categories.name;
-    })
-  );
+function generateAllCategories(works) {
+  btnFiltreesEverything.addEventListener("click", function () {
+    for (let work of works) generateWork(work);
+  });
+}
+
+function createElmFiltres(classe, categorie) {
+  const btnElm = createAppend(btnFiltres, "button");
+  btnElm.classList.add(classe);
+  btnElm.innerText = categorie;
+}
+
+createElmFiltres(".btn-objets", "Objets");
+createElmFiltres(".btn-appartements", "Appartements");
+createElmFiltres(".btn-hotelsRestaurants", "Hôtels & restaurants");
+btnFiltres.addEventListener("click", function () {
+  const categoriesFilter = works.filter(function (work) {
+    if (1 === works.id) {
+      return console.log(work.id);
+    }
+  });
 });
+
+/*function categoryObjets(categorie) {
+  const btnElm = createAppend(btnFiltres, "button");
+  const btnObjets = btnElm.classList.add("btn-objets");
+  btnElm.innerText = categorie.name;
+  btnObjets.addEventListener("click", function () {
+    const categoriesFilter = works.filter(function (categorie) {
+      return categorie.name === "Objets";
+    });
+  });
+}*/
 generateWorks(works);
+generateAllCategories(works);
