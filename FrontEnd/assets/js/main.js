6ecab96118bc07aaad67bf3bd5950b754bdf4d8c
Siyanne <sirayanne.ket@gmail.com>
2023-04-30 23:37:36 +0200

    categorie ok mais l'affichage ne marche pas et creation btn&class/ js avec classlist add => X

--- FrontEnd/assets/js/main.js
+++ FrontEnd/assets/js/main.js
@@ -6,7 +6,14 @@ const categories = await fetchCategories();
 const galleryElm = document.querySelector(".gallery");
 const btnFiltreesEverything = document.querySelector(".btn-trier-tous");
 const btnFiltres = document.querySelector(".filtres");
+galleryElm.classList.add(".gallery-show");
+const showWorks = document.querySelector(".gallery-show");
+galleryElm.classList.add(".gallery-zero");
 
+const btnObjets = document.querySelector(".btn-objets");
+const btnAppartements = document.querySelector(".btn-appartements");
+const btnHoAndResto = document.querySelector(".btn-hotelsRestaurants");
+const galleryZero = document.querySelector(".galleryZero");
 function createAppend(elm, tag) {
   return elm.appendChild(document.createElement(tag));
 }
@@ -23,14 +30,25 @@ function generateWork(work) {
 
 function generateWorks(works) {
   for (let work of works) generateWork(work);
-  document.querySelector(".gallery").innerHTML = " ";
+  //document.querySelector(".gallery").innerHTML = " ";
+}
+let isOpen = true;
+function generateAllCategories(works) {
+  for (let work of works) generateWork(work);
+  isOpen = true;
 }
 
-function generateAllCategories(works) {
-  btnFiltreesEverything.addEventListener("click", function () {
-    for (let work of works) generateWork(work);
-  });
+function generateAllCategoriesZero() {
+  document.querySelector(".gallery-zero").innerHTML = " ";
+  isOpen = false;
 }
+btnFiltreesEverything.addEventListener("click", function () {
+  if (isOpen == true) {
+    generateAllCategories(works);
+  } else {
+    generateAllCategoriesZero;
+  }
+});
 
 function createElmFiltres(classe, categorie) {
   const btnElm = createAppend(btnFiltres, "button");
@@ -38,26 +56,24 @@ function createElmFiltres(classe, categorie) {
   btnElm.innerText = categorie;
 }
 
-createElmFiltres(".btn-objets", "Objets");
-createElmFiltres(".btn-appartements", "Appartements");
-createElmFiltres(".btn-hotelsRestaurants", "Hôtels & restaurants");
-btnFiltres.addEventListener("click", function () {
-  const categoriesFilter = works.filter(function (work) {
-    if (1 === works.id) {
-      return console.log(work.id);
-    }
+btnObjets.addEventListener("click", function () {
+  const worksFiltrees = works.filter(function (work) {
+    return work.categoryId == 1;
   });
+  console.log(worksFiltrees);
 });
-
-/*function categoryObjets(categorie) {
-  const btnElm = createAppend(btnFiltres, "button");
-  const btnObjets = btnElm.classList.add("btn-objets");
-  btnElm.innerText = categorie.name;
-  btnObjets.addEventListener("click", function () {
-    const categoriesFilter = works.filter(function (categorie) {
-      return categorie.name === "Objets";
-    });
+createElmFiltres("btn-appartements", "Appartements");
+btnAppartements.addEventListener("click", function () {
+  const worksFiltrees = works.filter(function (work) {
+    return work.categoryId == 2;
   });
-}*/
+  console.log(worksFiltrees);
+});
+/*createElmFiltres("btn-hotelsRestaurants", "Hôtels & restaurants");
+btnHoAndResto.addEventListener("click", function () {
+  const worksFiltrees = works.filter(function (work) {
+    return work.categoryId == 3;
+  });
+});*/
+
 generateWorks(works);
-generateAllCategories(works);
