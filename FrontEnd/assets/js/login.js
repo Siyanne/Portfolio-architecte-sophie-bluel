f68e8551b06e838e2381c86174f666d8a72cea3b
Siyanne <sirayanne.ket@gmail.com>
2023-05-09 13:22:47 +0200

    login

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -1,5 +1,63 @@
-import { fetchLogin } from "./api.js";
-console.log(fetchLogin);
+console.log("helllo");
+//import { createAppend } from "./main.js";
+const emailInput = document.querySelector(".email");
+const pswInput = document.querySelector(".password");
+const logInBtn = document.querySelector(".connexion");
+const logoutLink = document.querySelector(".logout--link");
+const loginLink = document.querySelector(".login--link");
+
+async function fetchLogin() {
+  const userInfo = {
+    email: emailInput.value,
+    password: pswInput.value,
+  };
+  console.log(userInfo);
+  const reponse = await fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(userInfo),
+  });
+  const result = await reponse.json(); // problème au niveau du result
+  window.localStorage.setItem("admin", JSON.parse(result));
+  console.log(result);
+  const error = false;
+  if (result == true) {
+    window.sessionStorage.setItem("connected", true);
+    adminConnected();
+    navEdition();
+  } else {
+    loginError();
+  }
+}
+function toLogIn() {
+  logInBtn.addEventListener("click", () => fetchLogin());
+}
+function adminConnected() {
+  console.log("ça marche");
+  document.location.replace("index.html");
+  /*logoutBtn();
+  navEdition();*/
+}
+
+function navEdition() {
+  const editingMenu = document.querySelector(".editing");
+  editingMenu.classList.toggle("displayEdit");
+}
+/*function logoutBtn() {
+  loginLink.style.display === "none";
+  logoutLink.style.display === "block";
+}*/
+const msgError = document.querySelector(".errorMsg");
+
+function loginError() {
+  const errorElm = document.createElement("p");
+  errorElm.classList.add("msgErreur");
+  errorElm.innerText = "Erreur dans l’identifiant ou le mot de passe";
+  msgError.appendChild(errorElm);
+}
+/*import { fetchLogin } from "./api.js";
+import { createAppend } from "./main.js";*/
+/*console.log(fetchLogin);
 console.log("hello");
 const login = await fetchLogin();
 console.log(login);
@@ -10,10 +68,10 @@ const msgError = document.querySelector(".errorMsg");
 const loginMenu = document.querySelector(".login--link");
 const logout = document.querySelector(".logout--link");
 const errorElm = createAppend(msgError, ".errorMsg");
-console.log(emailValue);
+const editingMenu = document.querySelector(".editing");
 const userInfo = { email: email.value, password: psw.value };
 logInBtn.addEventListener("click", function () {
-  logIn();
+  trTogIn();
 });
 console.log(userInfo);
 console.log(login);
@@ -30,8 +88,8 @@ async function logInStorage() {
     loginError();
   }
 }
-function logIn() {
-  if (userInfo.email === login.email || userInfo.password !== login.password) {
+function tryToLogIn() {
+  if (userInfo.email === login.email && userInfo.password === login.password) {
     adminConnected();
   } else {
     errorElm.innerText = "“Erreur dans l’identifiant ou le mot de passe";
@@ -40,18 +98,12 @@ function logIn() {
 function adminConnected() {
   document.location.replace("index.html");
   logoutBtn();
+  navEdition();
 }
-function loginError() {
-  errorElm.innerHTML = "";
-}
-/*function userInfo() {
-  const emailValue = selectorEmail.value;
-  const pswValue = selectorPsw.value;
-  const userInfo = { email: emailValue, password: pswValue };
-  console.log(userInfo);
-}
-userInfo();*/
 
+function navEdition() {
+  editingMenu.style.display = "block";
+}
 function logoutBtn() {
   login.style.display = "none";
   logout.style.display = "block";
@@ -64,4 +116,9 @@ function logoutBtn() {
     window.location.replace("index.html");
   });
 }*/
-fetchLogin();
+/*logInStorage();
+tryToLogIn()*/
+//loginError();
+toLogIn();
+adminConnected();
+navEdition();
